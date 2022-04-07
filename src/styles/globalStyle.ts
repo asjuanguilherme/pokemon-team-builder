35401db30181eef01a56b8621e3bf41fdcc8d8b8
Juan Guilherme <asjuanguilherme@gmail.com>
2022-04-07 10:09:21 -0300

    feat: :sparkles: Implementação inicial do styled-components
    configuração do globalstyle do styled-components

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components"
+import { light } from "./themes"
+
+const GlobalStyle = createGlobalStyle`
+    html { background: ${light.colors.background} }
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+        border: 0;
+        font-family: 'Spartan', sans-serif;
+    }
+    
+    #root {
+        width: 100%;
+        color: ${light.colors.text};
+    }
+`
+
+export default GlobalStyle
---
+++ src/styles/themes.ts
@@ -0,0 +1,9 @@
+export const light = {
+  colors: {
+    primary: "#90ADC6",
+    text: "#333652",
+    background: "#E9EAEC",
+    success: "#8FDA58",
+    danger: "#F8635A",
+  },
+}
