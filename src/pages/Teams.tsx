edb7de49ca45a619199e28f2b7de1b84cdf3d720
Juan Guilherme <asjuanguilherme@gmail.com>
2022-04-10 14:15:07 -0300

    feature: :sparkles: implementação de scroll initito na listagem de times

--- src/pages/Teams.tsx
+++ src/pages/Teams.tsx
@@ -5,26 +5,35 @@ import Team from '../components/Team'
 import { fetchTeams } from '../services/teamsApi'
 
 const Home = () => {
-  const [teams, setTeams] = useState([])
+  const [teams, setTeams] = useState([] as Object[])
   const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
 
   useEffect(() => {
     setLoading(true)
-    fetchTeams()
-      .then(res => setTeams(res.data))
+    fetchTeams(currentPage)
+      .then(res => res.data)
+      .then(newTeams => setTeams(prevTeams => [...prevTeams, ...newTeams]))
       .catch(err => console.log(err))
       .finally(() => setLoading(false))
+  }, [currentPage])
+
+  useEffect(() => {
+    const intersectionObserverTeams = new IntersectionObserver(
+      (entries: any) => {
+        if (entries.some((entry: any) => entry.isIntersecting))
+          setCurrentPage((prevPage: number) => prevPage + 1)
+      }
+    )
+
+    intersectionObserverTeams.observe(
+      document.getElementById('team-list-sentinel') as Element
+    )
+    return () => intersectionObserverTeams.disconnect()
   }, [])
 
   return (
     <Layout linkTo="/" title="Create New Team">
-      {loading && (
-        <Loading
-          size="3rem"
-          message="Loading teams..."
-          style={{ paddingTop: '2rem' }}
-        />
-      )}
       {teams?.map((team: any, index) => (
         <Team
           name={team.name}
@@ -32,6 +41,19 @@ const Home = () => {
           key={index}
         />
       ))}
+      {teams && (
+        <span
+          style={{ paddingTop: '.5rem', display: 'block' }}
+          id="team-list-sentinel"
+        ></span>
+      )}
+      {loading && (
+        <Loading
+          size="3rem"
+          message="Loading teams..."
+          style={{ padding: '2rem 0' }}
+        />
+      )}
     </Layout>
   )
 }
--- src/services/teamsApi.ts
+++ src/services/teamsApi.ts
@@ -3,14 +3,16 @@ import qs from 'qs'
 import { PokemonTeamData } from '../types/pokemonTeam'
 
 export const teamsApi = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'http://localhost:4000/',
   headers: { 'content-type': 'application/x-www-form-urlencoded' },
 })
 
 export const sendNewTeam = async (team: PokemonTeamData) => {
-  return await teamsApi.post('/teams', qs.stringify(team))
+  return await teamsApi.post('teams', qs.stringify(team))
 }
 
-export const fetchTeams = async () => {
-  return await teamsApi.get('/teams')
+export const fetchTeams = async (page: number, perPage: number = 2) => {
+  return await teamsApi.get(
+    `teams?offset=${(page - 1) * perPage}&limit=${perPage}`
+  )
 }
