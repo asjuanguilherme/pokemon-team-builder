6cd246edb7a7df491abd08b4d906e28a6df711a2
Juan Guilherme <asjuanguilherme@gmail.com>
2022-04-10 00:49:27 -0300

    feat: Componente de Loading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Layout from '../components/Layout'
+import Loading from '../components/Loading'
 import PokemonList from '../components/PokemonList'
 import TeamBuilder from '../components/TeamBuilder'
 
 const Home = () => {
   return (
     <Layout linkTo="/teams" title="Teams">
+      <Loading size="3rem" />
       <TeamBuilder />
       <PokemonList />
     </Layout>
